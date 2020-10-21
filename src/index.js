eb7e71e95dbbeb3867e1cc813a8e553a24c3fa51
danilovaks <49689868+danilovaks@users.noreply.github.com>
2020-10-21 20:40:54 +0300

    feat: ready task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let reverseNum = n.toString().split('').reverse('').join('');
+
+  if(n < 0) {
+    reverseNum = reverseNum.slice(0, -1);
+    return +reverseNum;
+  }
+
+  return +reverseNum;
 }
